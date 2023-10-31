741b47b1c99a4264ce6a776007dd60b6a24fc848
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-10-31 17:10:20 -0400

    Updated their implementation to include parent node and lists that store animals and statements used

--- animals/BinaryTree.java
+++ animals/BinaryTree.java
@@ -1,14 +1,20 @@
 package animals;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class BinaryTree {
 
     private Node root;
+    private SortedSet<String> animals;
+    private Set<String> facts;
 
-    public BinaryTree(Node root) {
-        this.root = root;
+    public BinaryTree() {
+        this.animals = new TreeSet<String>();
+        this.facts = new HashSet<String>();
     }
-
-    public BinaryTree() {}
 
     public Node getRoot() {
         return this.root;
@@ -16,6 +22,45 @@ public class BinaryTree {
 
     public void setRoot(Node root) {
         this.root = root;
+    }
+
+    public SortedSet<String> getAnimals() {
+        return this.animals;
+    }
+
+    public void setAnimals(SortedSet<String> animals) {
+        this.animals = animals;
+    }
+
+    public Set<String> getFacts() {
+        return this.facts;
+    }
+
+    public void setFacts(Set<String> facts) {
+        this.facts = facts;
+    }
+
+    // Returns null if value is not in BinaryTree
+    public Node findNode(String value) {
+        return findNodeHelper(value, this.root);
+    }
+
+    private static Node findNodeHelper(String value, Node root) {
+        if (root == null) {
+            return null;
+        } else if (Main.stripArticle(root.getValue()).equals(value)) {
+            return root;
+        } else {
+            if (root.getLeft() != null) {
+                Node possible = findNodeHelper(value, root.getLeft());
+                if (possible != null) return possible;
+            }
+            if (root.getRight() != null) {
+                Node possible = findNodeHelper(value, root.getRight());
+                if (possible != null) return possible;
+            }
+        }
+        return null;
     }
 
 }
--- animals/Node.java
+++ animals/Node.java
@@ -1,16 +1,21 @@
 package animals;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Node implements Serializable {
+    private static final long serialVersionUID = 7L;
     private String value;
     private Node left;  // represents the result of answering "no"
     private Node right;  // represents the result of answering "yes"
+    private Node parent;
 
     public Node(String value) {
         this.value = value;
         this.left = null;
         this.right = null;
+        this.parent = null;
     }
 
     public String getValue() {
@@ -25,21 +30,17 @@ public class Node implements Serializable {
         return this.right;
     }
 
-    public void setLeft(String value) {
-        this.left = new Node(value);
-    }
-
     public void setLeft(Node leftNode) {
         this.left = leftNode;
-    }
-
-    public void setRight(String value) {
-        this.right = new Node(value);
     }
 
     public void setRight(Node rightNode) {
         this.right = rightNode;
     }
+
+    public void setParent(Node parentNode) { this.parent = parentNode; }
+
+    public Node getParent() { return this.parent; }
 
     @Override
     public boolean equals(Object obj) {
@@ -52,14 +53,36 @@ public class Node implements Serializable {
 
         // Create the question Node
         Node questionNode = new Node(animalFact);
+
         questionNode.setLeft(new Node(noAnimal));
         questionNode.setRight(new Node(yesAnimal));
+
+        questionNode.getLeft().setParent(questionNode);
+        questionNode.getRight().setParent(questionNode);
 
         return questionNode;
     }
 
     public boolean isAnimalNode() {
         return this.left == null && this.right == null;
+    }
+
+    public List<String> getFacts() {
+        List<String> parents = new ArrayList<>();
+        Node parentNode = this.getParent();
+        Node currentNode = this;
+
+        while (parentNode != null) {
+            String animalFact = parentNode.getValue();
+            if (parentNode.getLeft().equals(currentNode)) animalFact = Main.getNegationFromFact(animalFact);
+            if (!parents.isEmpty()) parents.add(0, animalFact);
+            else parents.add(animalFact);
+
+            currentNode = parentNode;
+            parentNode = parentNode.getParent();
+        }
+
+        return parents;
     }
 
 }
